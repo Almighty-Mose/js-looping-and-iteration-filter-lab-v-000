4024153dca804739b6e548624552c7ce051f0a89
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-12 21:02:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers, query) {
-   return drivers.filter(function (name) {return name === query;});
+   return drivers.filter(function (name) {return name.toLowerCase() === query.toLowerCase();});
 }
