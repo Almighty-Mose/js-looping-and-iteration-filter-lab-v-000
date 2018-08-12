c538ef8bffeb2b7be23da39cc8e8c0c0fa80ab5c
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-12 21:05:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Code your solution in this file
 function findMatching(drivers, query) {
    return drivers.filter(function (name) {
-     return name.toLowerCase() === query.toLowerCase();});
+     return name.toLowerCase() === query.toLowerCase();
+   });
 }
 
 function fuzzyMatch(drivers, query) {
